9cd3b6c47dc03f39c3f7626b673e76cbf16daaf0
IMChiLe <1297091087@qq.com>
2019-06-14 10:46:25 +0800

    IMChiLe

--- app/src/main/java/news/tencent/charco/android/view/fragment/CodeLoginActivity.java
+++ app/src/main/java/news/tencent/charco/android/view/fragment/CodeLoginActivity.java
@@ -3,20 +3,28 @@ package news.tencent.charco.android.view.fragment;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import news.tencent.charco.android.R;
+import news.tencent.charco.android.view.activity.SMSActivity;
+
+import static news.tencent.charco.android.NewsApplication.getContext;
 
 public class CodeLoginActivity extends AppCompatActivity {
 
     private TimeCount time;
     private Button btnGetcode;
-
-
+    private EditText codeName;
+    private EditText codePassword;
+    private Button codeLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,10 +42,42 @@ public class CodeLoginActivity extends AppCompatActivity {
 
         time = new TimeCount(60000, 1000);
         btnGetcode=(Button) findViewById(R.id.getCode);
+        codeName = findViewById(R.id.codeName);
+        codeLogin = findViewById(R.id.codeLogin);
+        codePassword = findViewById(R.id.codePassword);
         btnGetcode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                time.start();
+                if(codeName.length()==0 || codeName==null){
+                    Toast.makeText(CodeLoginActivity.this,"请输入相关信息",Toast.LENGTH_LONG).show();
+                }else {
+                    time.start();
+                    final String s = codeName.getText().toString();
+                    System.out.println(s);
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            SMSActivity smsActivity = new SMSActivity();
+                            smsActivity.getCode(s);
+                            Looper.prepare();
+                            new Handler().post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(getContext(),"发送成功,请注意接收",Toast.LENGTH_LONG).show();
+                                }
+                            });
+                            Looper.loop();
+                        }
+                    }).start();
+                }
+            }
+        });
+
+        codeLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone = codeName.getText().toString();
+                String code = codePassword.getText().toString();
             }
         });
     }
